b198cd8d7fa68fd5464deb4d18b39edf4c459ac0
Gerard Klijs <g.klijs@gmail.com>
2020-09-02 23:45:11 +0200

    No Class but Struct

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 use rand::Rng;
 
+#[derive(Debug)]
+struct NoClass{
+    x: f64,
+    y: f64,
+}
+
 fn main() {
     let mut rng = rand::thread_rng();
     let x: f64 = rng.gen();
     let y: f64 = rng.gen();
-    println!("Two random floats, {} and {}", x, y);
+    let no_class = NoClass{x,y};
+    println!("No class: {:?}", no_class);
 }
