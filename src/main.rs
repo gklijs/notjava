9c15146a9197d825e25b4167ba754a32d065c37b
Gerard Klijs <g.klijs@gmail.com>
2020-09-02 23:53:55 +0200

    No interface but trait

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use core::fmt;
 
 #[derive(Debug)]
 struct NoClass{
@@ -6,10 +7,17 @@ struct NoClass{
     y: f64,
 }
 
+// Implement `Display` for `NoClass`.
+impl fmt::Display for NoClass {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "just two random numbers x:{} and y:{}", self.x, self.y)
+    }
+}
+
 fn main() {
     let mut rng = rand::thread_rng();
     let x: f64 = rng.gen();
     let y: f64 = rng.gen();
     let no_class = NoClass{x,y};
-    println!("No class: {:?}", no_class);
+    println!("No class: {}", no_class);
 }
